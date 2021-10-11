0d86a32ba3a62d130a9bb365645d69f545631c9c
Neto1904 <netohsa66@gmail.com>
2021-10-11 17:53:35 -0300

    Remove ormconfig from git

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { routes } from './routes/index.routes'
 
 const app = express()
 app.use(express.json())
-app.use(routes)
+app.use('/api', routes)
 app.use(checkError)
 
 app.listen(3000, () => {
