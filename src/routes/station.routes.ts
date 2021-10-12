81925af96d8348f397a5715723d1cb718e09bca6
Neto1904 <netohsa66@gmail.com>
2021-10-12 10:36:26 -0300

    Add count by responsible route

--- src/routes/station.routes.ts
+++ src/routes/station.routes.ts
@@ -1,5 +1,6 @@
 import { CountAllStationsController } from '@modules/station/services/countAllStations/CountAllStationsController'
 import { CountStationsByCountryController } from '@modules/station/services/countStationsByCountry/CountStationsByCountryController'
+import { CountStationsByResponsibleController } from '@modules/station/services/countStationsByResponsible/CountStationsByResponsibleController'
 import { CountStationsBySubwatershedController } from '@modules/station/services/countStationsBySubwatershed/CountStationsBySubwatershedController'
 import { CountStationsByTypeController } from '@modules/station/services/countStationsByType/CountStationsByTypeController'
 import { GetStationsPointsController } from '@modules/station/services/getStationsPoints/GetStationsPointsController'
@@ -11,12 +12,18 @@ const countAllStationsContoller = new CountAllStationsController()
 const countStationsByType = new CountStationsByTypeController()
 const countStationsByCountry = new CountStationsByCountryController()
 const countStationsBySubwatershed = new CountStationsBySubwatershedController()
+const countStationsByResponsibleController =
+  new CountStationsByResponsibleController()
 const getStationsPointsController = new GetStationsPointsController()
 
 stationRoutes.get('/count', countAllStationsContoller.handle)
 stationRoutes.get('/count/type', countStationsByType.handle)
 stationRoutes.get('/count/country', countStationsByCountry.handle)
 stationRoutes.get('/count/subwatershed', countStationsBySubwatershed.handle)
+stationRoutes.get(
+  '/count/responsible',
+  countStationsByResponsibleController.handle
+)
 stationRoutes.get('/shape', getStationsPointsController.handle)
 
 export { stationRoutes }
