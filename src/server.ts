7c685efc8d6d92ece07ca0f1909a515f690f42b2
Neto1904 <netohsa66@gmail.com>
2021-10-12 10:33:33 -0300

    Use env port to execute the server

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,6 @@ app.use(express.json())
 app.use('/api', routes)
 app.use(checkError)
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Server is running on port ${process.env.APP_PORT}`)
 })
