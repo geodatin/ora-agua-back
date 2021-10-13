9aa3aa168dc94bd61b599bae17598cd3addf6837
Neto1904 <netohsa66@gmail.com>
2021-10-13 11:50:37 -0300

    Use cors middlware on the server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import 'express-async-errors'
 
@@ -9,6 +10,7 @@ import { routes } from './routes/index.routes'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 app.use('/api', routes)
 app.use(checkError)
 
