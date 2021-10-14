9db7ea63520e323c29beb5480308d86a9089d37e
Neto1904 <netohsa66@gmail.com>
2021-10-14 10:46:45 -0300

    Fix float pg response

--- src/utils/formatPgResponse.ts
+++ src/utils/formatPgResponse.ts
@@ -5,7 +5,7 @@ types.setTypeParser(types.builtins.INT8, (value: string) => {
 })
 
 types.setTypeParser(types.builtins.FLOAT8, (value: string) => {
-  return parseFloat(value)
+  return parseFloat(parseFloat(value).toFixed(2))
 })
 
 types.setTypeParser(types.builtins.NUMERIC, (value: string) => {
