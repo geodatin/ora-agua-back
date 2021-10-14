11acb7bfc7e10f77268d5e342555fd7e390326f9
Neto1904 <netohsa66@gmail.com>
2021-10-14 10:46:00 -0300

    Add variance ranking method

---
+++ src/modules/water/services/varianceRankingService/VarianceRankingService.ts
@@ -0,0 +1,34 @@
+import { IVarianceRankingDTO } from '@modules/water/dtos/IVarianceRankingDTO'
+import { IWaterAreaRepository } from '@modules/water/repositories/IWaterAreaRepository'
+import { inject, injectable } from 'tsyringe'
+
+@injectable()
+class VarianceRankingService {
+  constructor(
+    @inject('WaterAreaRepository')
+    private waterAreaRepository: IWaterAreaRepository
+  ) {}
+
+  async execute({ order, initialYear, finalYear }: IVarianceRankingDTO) {
+    const result = await this.waterAreaRepository.varianceRanking({
+      initialYear,
+      finalYear,
+      order,
+    })
+    const x: string[] = []
+    const y: number[] = []
+    const pos: number[] = []
+    for (const [index, element] of result.entries()) {
+      x.push(element.name)
+      y.push(element.sum)
+      pos.push(index + 1)
+    }
+    return {
+      x: x,
+      series: [{ id: 'station', data: y }],
+      position: pos,
+    }
+  }
+}
+
+export { VarianceRankingService }
