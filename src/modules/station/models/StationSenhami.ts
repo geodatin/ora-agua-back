e50ab810cda0b17bfc676a2324e67ba5e22854b7
Neto1904 <netohsa66@gmail.com>
2021-12-16 11:23:19 -0300

    Add new models

---
+++ src/modules/station/models/StationSenhami.ts
@@ -0,0 +1,22 @@
+import { Point } from 'geojson'
+import { Column, Entity, PrimaryColumn } from 'typeorm'
+
+@Entity('station_senhami')
+class StationSenhami {
+  @PrimaryColumn()
+  code: string
+
+  @Column()
+  name: string
+
+  @Column({ type: 'geometry', srid: 4326, nullable: true })
+  location: Point
+
+  @Column()
+  city: string
+
+  @Column('double precision')
+  height: number
+}
+
+export { StationSenhami }
