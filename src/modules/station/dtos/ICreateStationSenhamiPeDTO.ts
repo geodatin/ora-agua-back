4810ec1f7ff17f7fa46b267af9b629d94f352fa9
Neto1904 <netohsa66@gmail.com>
2021-12-22 15:02:34 -0300

    Add station senhami pe dto

---
+++ src/modules/station/dtos/ICreateStationSenhamiPeDTO.ts
@@ -0,0 +1,11 @@
+interface ICreateStationSenhamiPeDTO {
+  code: string
+  name: string
+  longitude: string
+  latitude: string
+  oldCode: string
+  type: string
+  cate: string
+}
+
+export { ICreateStationSenhamiPeDTO }
