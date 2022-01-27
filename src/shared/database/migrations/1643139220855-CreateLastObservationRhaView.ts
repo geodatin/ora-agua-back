9e7ad26beada5ee1fab7fd5d5cb13362733d72d4
Neto1904 <netohsa66@gmail.com>
2022-01-27 15:54:15 -0300

    Chage view definition

--- src/shared/database/migrations/1643139220855-CreateLastObservationRhaView.ts
+++ src/shared/database/migrations/1643139220855-CreateLastObservationRhaView.ts
@@ -7,70 +7,70 @@ export class CreateLastObservationRhaView1643139220855
     await queryRunner.query(
       `CREATE MATERIALIZED VIEW last_observation_rha_view AS (
         SELECT SUM(rain) rain, AVG(flow_rate) flow_rate, AVG(level) AS level, o.station_code, last_update,
-                '1 HOUR' as interval
-                FROM observation_rha_view o
-                INNER JOIN (
-                SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
-                GROUP BY station_code
-            ) o2 ON o.station_code = o2.station_code
-                WHERE timestamp > (
-                    SELECT MAX(timestamp) FROM observation_rha_view 
-                )- INTERVAL '1 HOUR' GROUP BY o.station_code, o2.last_update
+            'hour' as frequency
+            FROM observation_rha_view o
+            INNER JOIN (
+            SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
+            GROUP BY station_code
+        ) o2 ON o.station_code = o2.station_code
+            WHERE timestamp > (
+                SELECT MAX(timestamp) FROM observation_rha_view 
+            )- INTERVAL '1 HOUR' GROUP BY o.station_code, o2.last_update
         UNION
         SELECT SUM(rain) rain, AVG(flow_rate) flow_rate, AVG(level) AS level, o.station_code, last_update,
-                '1 DAY' as interval
-                FROM observation_rha_view o
-                INNER JOIN (
-                SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
-                GROUP BY station_code
-            ) o2 ON o.station_code = o2.station_code
-                WHERE timestamp > (
-                    SELECT MAX(timestamp) FROM observation_rha_view 
-                )- INTERVAL '1 DAY' GROUP BY o.station_code, o2.last_update
+            'day' as frequency
+            FROM observation_rha_view o
+            INNER JOIN (
+            SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
+            GROUP BY station_code
+        ) o2 ON o.station_code = o2.station_code
+            WHERE timestamp > (
+                SELECT MAX(timestamp) FROM observation_rha_view 
+            )- INTERVAL '1 DAY' GROUP BY o.station_code, o2.last_update
         UNION
         SELECT SUM(rain) rain, AVG(flow_rate) flow_rate, AVG(level) AS level, o.station_code, last_update,
-                '1 WEEK' as interval
-                FROM observation_rha_view o
-                INNER JOIN (
-                SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
-                GROUP BY station_code
-            ) o2 ON o.station_code = o2.station_code
-                WHERE timestamp > (
-                    SELECT MAX(timestamp) FROM observation_rha_view 
-                )- INTERVAL '1 WEEK' GROUP BY o.station_code, o2.last_update
+            'week' as frequency
+            FROM observation_rha_view o
+            INNER JOIN (
+            SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
+            GROUP BY station_code
+        ) o2 ON o.station_code = o2.station_code
+            WHERE timestamp > (
+                SELECT MAX(timestamp) FROM observation_rha_view 
+            )- INTERVAL '1 WEEK' GROUP BY o.station_code, o2.last_update
         UNION
         SELECT SUM(rain) rain, AVG(flow_rate) flow_rate, AVG(level) AS level, o.station_code, last_update,
-                '1 MONTH' as interval
-                FROM observation_rha_view o
-                INNER JOIN (
-                SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
-                GROUP BY station_code
-            ) o2 ON o.station_code = o2.station_code
-                WHERE timestamp > (
-                    SELECT MAX(timestamp) FROM observation_rha_view 
-                )- INTERVAL '1 MONTH' GROUP BY o.station_code, o2.last_update
+            'month' as frequency
+            FROM observation_rha_view o
+            INNER JOIN (
+            SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
+            GROUP BY station_code
+        ) o2 ON o.station_code = o2.station_code
+            WHERE timestamp > (
+                SELECT MAX(timestamp) FROM observation_rha_view 
+            )- INTERVAL '1 MONTH' GROUP BY o.station_code, o2.last_update
         UNION
         SELECT SUM(rain) rain, AVG(flow_rate) flow_rate, AVG(level) AS level, o.station_code, last_update,
-                '3 MONTHS' as interval
-                FROM observation_rha_view o
-                INNER JOIN (
-                SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
-                GROUP BY station_code
-            ) o2 ON o.station_code = o2.station_code
-                WHERE timestamp > (
-                    SELECT MAX(timestamp) FROM observation_rha_view 
-                )- INTERVAL '3 MONTHS' GROUP BY o.station_code, o2.last_update
+            'quarter' as frequency
+            FROM observation_rha_view o
+            INNER JOIN (
+            SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
+            GROUP BY station_code
+        ) o2 ON o.station_code = o2.station_code
+            WHERE timestamp > (
+                SELECT MAX(timestamp) FROM observation_rha_view 
+            )- INTERVAL '3 MONTHS' GROUP BY o.station_code, o2.last_update
         UNION
         SELECT SUM(rain) rain, AVG(flow_rate) flow_rate, AVG(level) AS level, o.station_code, last_update,
-                '1 YEAR' as interval
-                FROM observation_rha_view o
-                INNER JOIN (
-                SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
-                GROUP BY station_code
-            ) o2 ON o.station_code = o2.station_code
-                WHERE timestamp > (
-                    SELECT MAX(timestamp) FROM observation_rha_view 
-                )- INTERVAL '1 YEAR' GROUP BY o.station_code, o2.last_update
+            'year' as frequency
+            FROM observation_rha_view o
+            INNER JOIN (
+            SELECT MAX(timestamp) AS last_update, station_code FROM observation_rha_view
+            GROUP BY station_code
+        ) o2 ON o.station_code = o2.station_code
+            WHERE timestamp > (
+                SELECT MAX(timestamp) FROM observation_rha_view 
+            )- INTERVAL '1 YEAR' GROUP BY o.station_code, o2.last_update
         )`
     )
   }
