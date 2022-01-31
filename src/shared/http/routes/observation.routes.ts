8be1228cddcd4f94f904438ec0838f0f38e34949
Neto1904 <netohsa66@gmail.com>
2022-01-31 17:25:00 -0300

    Add route to last update method

--- src/shared/http/routes/observation.routes.ts
+++ src/shared/http/routes/observation.routes.ts
@@ -1,4 +1,5 @@
 import { LastObservationRhaController } from '@modules/observation/services/lastObservation/LastObservationRhaController'
+import { LastUpdateController } from '@modules/observation/services/lastUpdate/LastUpdateController'
 import { TimeSeriesController } from '@modules/observation/services/timeSeries/TimeSeriesController'
 import { Router } from 'express'
 
@@ -6,11 +7,14 @@ const observationRoutes = Router()
 
 const lastObservationRhaController = new LastObservationRhaController()
 const timeSeriesController = new TimeSeriesController()
+const lastUpdateControler = new LastUpdateController()
 
 observationRoutes.post('/last/:frequency', lastObservationRhaController.handle)
 observationRoutes.get(
   '/timeSeries/:stationCode/:dataType/:frequency',
   timeSeriesController.handle
 )
+
+observationRoutes.get('/lastUpdate', lastUpdateControler.handle)
 
 export { observationRoutes }
