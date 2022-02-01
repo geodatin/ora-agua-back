b6603d9d8f2c09ac176eab3b4341190ff643fd15
Neto1904 <netohsa66@gmail.com>
2022-02-01 15:12:11 -0300

    Add station situation

--- src/modules/station/services/getStationsPoints/GetStationsPointsService.ts
+++ src/modules/station/services/getStationsPoints/GetStationsPointsService.ts
@@ -12,10 +12,20 @@ class GetStationsPointsService {
   ) {}
 
   async execute(filters: IFiltersDTO, network?: string) {
-    const stations = await this.stationViewRepository.getStations(
+    const stations = (await this.stationViewRepository.getStations(
       filters,
       network
-    )
+    )) as any
+    stations.map((station) => {
+      if (station.rain > 100) {
+        station.situation = 'alert'
+      } else if (station.rain > 50) {
+        station.situation = 'attention'
+      } else {
+        station.situation = 'normal'
+      }
+      return station
+    })
     const parsed = geojson.parse(stations, { GeoJSON: 'location' })
     return parsed
   }
