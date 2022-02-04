4d6e63ab8932cd9a507159e0510647d94b7ae1c7
Neto1904 <netohsa66@gmail.com>
2022-02-04 14:30:39 -0300

    Add getStation DTO

---
+++ src/modules/station/dtos/IGetStationsDTO.ts
@@ -0,0 +1,25 @@
+import { Point } from 'geojson'
+
+import { IFiltersDTO } from './IFiltersDTO'
+
+export interface IGetStationsResponseDTO {
+  code: string
+  name: string
+  river: string
+  type: string
+  responsible: string
+  country: string
+  countryId: string
+  network: string
+  location: Point
+  rain: number
+  level: number
+  flowRate: number
+  situation: string
+  hasData: boolean
+}
+
+export interface IGetStationsRequestDTO {
+  filters: IFiltersDTO
+  network?: string
+}
