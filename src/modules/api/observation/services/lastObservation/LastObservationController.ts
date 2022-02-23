43da530590ac535393d25c55883c7ca33ed4b9dc
Neto1904 <netohsa66@gmail.com>
2022-02-23 15:50:59 -0300

    Change controller name

--- src/modules/api/observation/services/lastObservation/LastObservationController.ts
+++ src/modules/api/observation/services/lastObservation/LastObservationController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
 import { FrequencyType } from '../../types/FrequencyType'
-import { LastObservationRhaService } from './LastObservationRhaService'
+import { LastObservationService } from './LastObservationService'
 
-export class LastObservationRhaController {
+export class LastObservationController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { page, pageSize, stationCode } = request.query
     const { frequency } = request.params
     const { filters } = request.body
-    const service = container.resolve(LastObservationRhaService)
+    const service = container.resolve(LastObservationService)
     const data = await service.execute({
       page: page ? Number(page) : 1,
       pageSize: pageSize ? Number(pageSize) : 5,
