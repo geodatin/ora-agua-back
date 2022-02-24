d8ddf3d31b5058f52b7c81215481e3d692e909bf
Neto1904 <netohsa66@gmail.com>
2022-02-24 11:32:42 -0300

    Change controller name

--- src/modules/api/observation/services/listObservation/ListObservationController.ts
+++ src/modules/api/observation/services/listObservation/ListObservationController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
 import { FrequencyType } from '../../types/FrequencyType'
-import { LastObservationService } from './LastObservationService'
+import { ListObservationService } from './ListObservationService'
 
-export class LastObservationController {
+export class ListObservationController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { page, pageSize, stationCode } = request.query
     const { frequency } = request.params
     const { filters } = request.body
-    const service = container.resolve(LastObservationService)
+    const service = container.resolve(ListObservationService)
     const data = await service.execute({
       page: page ? Number(page) : 1,
       pageSize: pageSize ? Number(pageSize) : 5,
