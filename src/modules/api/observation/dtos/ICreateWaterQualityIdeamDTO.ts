915507648da525095fb0af1a6adb05213367306d
Neto1904 <netohsa66@gmail.com>
2022-03-01 11:58:09 -0300

    Remove unused files

--- src/modules/api/observation/dtos/ICreateWaterQualityIdeamDTO.ts
+++
@@ -1,9 +0,0 @@
-interface ICreateWaterQualityIdeamDTO {
-  stationCode: string
-  timestamp: string
-  totalNitrogen?: number
-  totalSuspensionSolid?: number
-  OD?: number
-}
-
-export { ICreateWaterQualityIdeamDTO }
