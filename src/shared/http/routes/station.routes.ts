50793d293388cc38696a94eea52c8faf076ff83a
Neto1904 <netohsa66@gmail.com>
2022-03-08 10:16:03 -0300

    Add new route

--- src/shared/http/routes/station.routes.ts
+++ src/shared/http/routes/station.routes.ts
@@ -7,6 +7,7 @@ import { CountStationsByTypeController } from '@modules/api/station/services/cou
 import { CountStatitonsByVariableController } from '@modules/api/station/services/countStationsByVariable/CountStationsByVariableController'
 import { GetFilterOptionsController } from '@modules/api/station/services/getFilterOptions/GetFilterOprionsController'
 import { GetNotificationsController } from '@modules/api/station/services/getNotifications/GetNotificationsController'
+import { GetProjectedStationsPointsController } from '@modules/api/station/services/getProjectedStationsPoints/GetProjectedStationsPointsController'
 import { GetStationsPointsController } from '@modules/api/station/services/getStationsPoints/GetStationsPointsController'
 import { RankingRiversByStationsController } from '@modules/api/station/services/rankingRiversByStations/RankingRiversByStationsController'
 import { Router } from 'express'
@@ -27,6 +28,8 @@ const rankingRiversByStationsController =
   new RankingRiversByStationsController()
 const getFilterOptionsController = new GetFilterOptionsController()
 const getNotificationsController = new GetNotificationsController()
+const getProjectedStationsPointsController =
+  new GetProjectedStationsPointsController()
 
 stationRoutes.post('/count', countAllStationsContoller.handle)
 stationRoutes.post('/count/type', countStationsByType.handle)
@@ -39,6 +42,10 @@ stationRoutes.post(
 stationRoutes.post('/count/network', countStationsByNetworkController.handle)
 stationRoutes.post('/count/variable', countStationsByVariablesController.handle)
 stationRoutes.post('/location', getStationsPointsController.handle)
+stationRoutes.get(
+  '/projected/location',
+  getProjectedStationsPointsController.handle
+)
 stationRoutes.post('/notification', getNotificationsController.handle)
 stationRoutes.post('/ranking/river', rankingRiversByStationsController.handle)
 stationRoutes.get('/filter/:filterTerm', getFilterOptionsController.handle)
