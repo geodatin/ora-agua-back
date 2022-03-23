b1b1cde96ea4d1a87811a5daa083107b35c7d09f
Neto1904 <netohsa66@gmail.com>
2022-03-23 20:39:37 -0300

    Aplly filter when necessary

--- src/modules/api/station/services/getProjectedStationsPoints/GetProjectedStationsPointsService.ts
+++ src/modules/api/station/services/getProjectedStationsPoints/GetProjectedStationsPointsService.ts
@@ -11,9 +11,19 @@ export class GetProjectedStationsPointsService {
     private stationViewRepository: IStationViewRepository
   ) {}
 
-  async execute() {
-    const stations = await this.stationViewRepository.getProjectedStations()
-
+  async execute(network: string) {
+    let stations
+    if (network === 'rha') {
+      stations = await this.stationViewRepository.getProjectedRhaStations()
+    } else if (network === 'rqa') {
+      stations = await this.stationViewRepository.getProjectedRqaStations()
+    } else {
+      const stationsRqa =
+        await this.stationViewRepository.getProjectedRqaStations()
+      const stationsRha =
+        await this.stationViewRepository.getProjectedRhaStations()
+      stations = stationsRha.concat(stationsRqa)
+    }
     const parsed = geojson.parse(stations, { GeoJSON: 'location' })
     return parsed
   }
