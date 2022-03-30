0cb9613cc6eb54df5c72da56bbf9bd0480f5d311
Neto1904 <netohsa66@gmail.com>
2022-03-30 17:14:57 -0300

    Add view entity

---
+++ src/modules/api/station/models/StationLimitView.ts
@@ -0,0 +1,67 @@
+import { ViewColumn, ViewEntity } from 'typeorm'
+
+@ViewEntity({
+  name: 'station_limit_view',
+  materialized: true,
+  schema: process.env.SCHEMA,
+  expression: `
+    SELECT (
+        SELECT MAX(value) AS flow_rate_inferior_limit
+        FROM (
+            SELECT flow_rate AS value, PERCENT_RANK() 
+                OVER (ORDER BY flow_rate) AS percentage 
+                FROM observation_rha_view observation 
+                WHERE flow_rate IS NOT NULL AND flow_rate != 0 AND station_code = s.code
+            ) t1 
+        WHERE percentage < 0.1
+    ) AS flow_rate_inferior_limit,
+    (
+        SELECT MIN(value) AS flow_rate_superior_limit
+        FROM (
+            SELECT flow_rate AS value, PERCENT_RANK() 
+                OVER (ORDER BY flow_rate) AS percentage 
+                FROM observation_rha_view observation 
+                WHERE flow_rate IS NOT NULL AND flow_rate != 0 AND station_code = s.code
+            ) t1 
+        WHERE percentage > 0.9
+    ) AS flow_rate_superior_limit,
+    (
+        SELECT MAX(value) AS level_inferior_limit
+        FROM (
+            SELECT level AS value, PERCENT_RANK() 
+                OVER (ORDER BY level) AS percentage 
+                FROM observation_rha_view observation 
+                WHERE level IS NOT NULL AND level != 0 AND station_code = s.code
+            ) t1 
+        WHERE percentage < 0.1
+    ) AS level_inferior_limit,
+    (
+        SELECT MIN(value) AS level_superior_limit
+        FROM (
+            SELECT level AS value, PERCENT_RANK() 
+                OVER (ORDER BY level) AS percentage 
+                FROM observation_rha_view observation 
+                WHERE level IS NOT NULL AND level != 0 AND station_code = s.code
+            ) t1 
+        WHERE percentage > 0.9
+    ) AS level_superior_limit,
+    code AS station_code
+    FROM station_view s
+  `,
+})
+export class StationLimitView {
+  @ViewColumn({ name: 'station_code' })
+  stationCode: string
+
+  @ViewColumn({ name: 'flow_rate_inferior_limit' })
+  flowRateInferiorLimit: number
+
+  @ViewColumn({ name: 'flow_rate_superior_limit' })
+  flowRateSuperiorLimit: number
+
+  @ViewColumn({ name: 'level_inferior_limit' })
+  levelInferiorLimit: number
+
+  @ViewColumn({ name: 'level_superior_limit' })
+  levelSuperiorLimit: number
+}
