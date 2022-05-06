c5ad38cf62693afa77b2412bee768d9f5921a61b
Neto1904 <netohsa66@gmail.com>
2022-05-06 09:47:47 -0300

    fix: refresh views from new schema after seed

--- src/modules/collector/general/repositories/typeorm/GeneralCollectorRepository.ts
+++ src/modules/collector/general/repositories/typeorm/GeneralCollectorRepository.ts
@@ -4,16 +4,22 @@ import { IGeneralCollectorRepository } from '../IGeneralCollectorRepository'
 
 export class GeneralCollectorRepository implements IGeneralCollectorRepository {
   async refreshViews(): Promise<void> {
-    await getManager().query('REFRESH MATERIALIZED VIEW station_view')
-
-    await getManager().query('REFRESH MATERIALIZED VIEW observation_rha_view')
+    await getManager().query('REFRESH MATERIALIZED VIEW network.station_view')
 
     await getManager().query(
-      'REFRESH MATERIALIZED VIEW observation_rha_list_view'
+      'REFRESH MATERIALIZED VIEW network.observation_rha_view'
     )
 
-    await getManager().query('REFRESH MATERIALIZED VIEW last_update_view')
+    await getManager().query(
+      'REFRESH MATERIALIZED VIEW network.observation_rha_list_view'
+    )
 
-    await getManager().query('REFRESH MATERIALIZED VIEW observation_rqa_view')
+    await getManager().query(
+      'REFRESH MATERIALIZED VIEW network.last_update_view'
+    )
+
+    await getManager().query(
+      'REFRESH MATERIALIZED VIEW network.observation_rqa_view'
+    )
   }
 }
