b86b56c8195c07ce36b7970473a755c379e1fca1
Neto1904 <netohsa66@gmail.com>
2022-06-06 09:51:44 -0300

    feat: add new methods to repository

--- src/modules/collector/ana/observation/repositories/IObservationRepository.ts
+++ src/modules/collector/ana/observation/repositories/IObservationRepository.ts
@@ -3,7 +3,9 @@ import { ICreateObservationDTO } from '../dtos/ICreateObservationDTO'
 interface IObservationRepository {
   create(data: ICreateObservationDTO): Promise<void>
   createMany(data: ICreateObservationDTO[]): Promise<void>
+  createManyOrUpdate(data: ICreateObservationDTO[]): Promise<void>
   getStationMaxDate(stationCode: number): Promise<string>
   refreshLastObservationView(): Promise<void>
+  deleteObservations(stationCode: number): Promise<void>
 }
 export { IObservationRepository }
