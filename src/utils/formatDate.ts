17cced4930d0b1de53bab361d83846d9e2c9abc0
Neto1904 <netohsa66@gmail.com>
2022-06-06 09:54:12 -0300

    feat: add new method to utils

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -7,3 +7,8 @@ export function formatDateSenhami(date: string): string {
   const [year, month, day] = date.split('/')
   return `${year}-${month}-${day}`
 }
+
+export function formatDateCsvs(date: string): string {
+  const [day, month, year] = date.split('/')
+  return day === '01' ? `${year}-${month}-01` : null
+}
