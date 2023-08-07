be8dff8e52b98bad11947fe7f0de8fa0ce9ca0fd
Neto1904 <netohsa66@gmail.com>
2023-08-07 11:03:33 -0300

    Add route to download station data

--- src/shared/http/routes/observation.routes.ts
+++ src/shared/http/routes/observation.routes.ts
@@ -1,3 +1,4 @@
+import { DownloadObservationsController } from '@modules/api/observation/services/downloadObservations/DownloadObservationsController'
 import { LastUpdateController } from '@modules/api/observation/services/lastUpdate/LastUpdateController'
 import { ListObservationController } from '@modules/api/observation/services/listObservation/ListObservationController'
 import { TimeSeriesController } from '@modules/api/observation/services/timeSeries/TimeSeriesController'
@@ -7,14 +8,18 @@ const observationRoutes = Router()
 
 const listObservationController = new ListObservationController()
 const timeSeriesController = new TimeSeriesController()
-const lastUpdateControler = new LastUpdateController()
+const lastUpdateController = new LastUpdateController()
+const downloadObservationsController = new DownloadObservationsController()
 
 observationRoutes.post('/list/:frequency', listObservationController.handle)
 observationRoutes.get(
   '/timeSeries/:network/:stationCode/:dataType/:frequency',
   timeSeriesController.handle
 )
-
-observationRoutes.get('/lastUpdate', lastUpdateControler.handle)
+observationRoutes.get('/lastUpdate', lastUpdateController.handle)
+observationRoutes.get(
+  '/timeSeries/:stationCode',
+  downloadObservationsController.handle
+)
 
 export { observationRoutes }
